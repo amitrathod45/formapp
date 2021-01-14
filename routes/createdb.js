4261dfd79033540b87fce62871b01265cc47d0a9
amitrathod45 <amitrathod211@yahoo.com>
2021-01-15 01:38:35 +0530

    first commit

---
+++ routes/createdb.js
@@ -0,0 +1,11 @@
+const nano = require("nano")
+
+exports.create = function(req,res){
+    nano.db.create(req.body.dbname,function(){
+        if(err){
+            res.send('error creating the database');
+            return;
+        }
+        res.send('Database created successfully')
+    });
+};
